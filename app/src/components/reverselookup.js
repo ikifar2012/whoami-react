b828511c5e32d557dd2d2d539ea121b321afcfb7
Matheson Steplock <ikifar2012@users.noreply.github.com>
2021-08-30 21:33:22 +0000

    fixing bad regex?

--- app/src/components/reverselookup.js
+++ app/src/components/reverselookup.js
@@ -35,7 +35,7 @@ class Posts extends Component {
   }
 
   handleSubmit(event) { // eslint-disable-next-line
-    var re = new RegExp("^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(?!$)|$)){4}$");
+    var re = new RegExp("^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(.(?!$)|$)){4}$");
     if (re.test(this.state.value)) {
     //   axios.get('https://ip-api.com/json/'+this.state.value)
     //   .then(
